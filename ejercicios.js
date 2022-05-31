c013d27c1b64420f7c1cc6f927a63f366cc7052f
AlanEstuardoSacalxotTacan <99003984+AlanEstuardoSacalxotTacan@users.noreply.github.com>
2022-05-31 19:50:49 +0200

    Ejercicio js 1

---
+++ ejercicios.js
@@ -0,0 +1,55 @@
+dinero1=prompt("¿Cuánto dinero tienes?");
+dinero2=prompt("¿Cuánto dinero tienes?");
+dinero3=prompt("¿Cuánto dinero tienes?");
+
+
+if (dinero1>=0.6 && dinero1<1){
+alert("Compra la opcion 1")
+}
+else if(dinero1>=1 && dinero1<2){
+    alert("la opcion 1 y 2 estan disponibles para comprar")
+}
+else if(dinero1>=2 && dinero1<3){
+    alert("la opcion 1, 2 y 3 estan disponibles para comprar")
+}
+else if(dinero1>=3){
+    alert("todas las opciones estan disponibles para comprar")
+}
+else {
+    alert("Lo sentimos, no tienes suficiente dinero para compar")
+}
+
+
+if (dinero2>=0.6 && dinero2<1){
+alert("Compra la opcion 1")
+}
+else if(dinero2>=1 && dinero2<2){
+    alert("la opcion 1 y 2 estan disponibles para comprar")
+}
+else if(dinero2>=2 && dinero2<3){
+    alert("la opcion 1, 2 y 3 estan disponibles para comprar")
+}
+else if(dinero2>=3){
+    alert("todas las opciones estan disponibles para comprar")
+}
+else {
+    alert("Lo sentimos, no tienes suficiente dinero para compar")
+}
+
+if (dinero3>=0.6 && dinero3<1){
+alert("Compra la opcion 1")
+}
+else if(dinero3>=1 && dinero3<2){
+    alert("la opcion 1 y 2 estan disponibles para comprar")
+}
+else if(dinero3>=2 && dinero3<3){
+    alert("la opcion 1, 2 y 3 estan disponibles para comprar")
+}
+else if(dinero3>=3){
+    alert("todas las opciones estan disponibles para comprar")
+}
+else {
+    alert("Lo sentimos, no tienes suficiente dinero para compar")
+}
+
+
